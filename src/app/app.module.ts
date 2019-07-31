c1e91951c47f023153dd8ac54c128ae867f44db1
Jovin James <Jovin_James@swissre.com>
2019-07-31 20:11:39 +0530

    layout changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { AvatarModule } from 'ngx-avatar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AvatarModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
